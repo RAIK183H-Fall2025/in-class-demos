5f521c24238efaa83faf204df6876cdf9f699497
stephanievalentine <stephanievalentine@users.noreply.github.com>
2025-09-29 11:48:49 -0500

    loops II

--- src/edu/unl/raikes/functions/tests/DiceRollerTest.java
+++ src/edu/unl/raikes/functions/tests/DiceRollerTest.java
@@ -1,5 +1,12 @@
 package edu.unl.raikes.functions.tests;
 
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class DiceRollerTest {
+    @Test
+    public void DiceRollerTest() {
+        Assertions.fail();
+    }
 
 }
